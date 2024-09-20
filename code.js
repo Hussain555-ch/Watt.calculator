623e4fd60abdb56d2c1a3df699e3ee5dbf2594a1
hussain555-ch <zainch786590@gmail.com>
2024-09-20 16:10:53 +0500

    change in code.js

--- code.js
+++ code.js
@@ -1,19 +1,19 @@
 function calculateWattage() {
-    const ac1_5Tun = parseFloat(document.getElementById("ac 1.5 tun").value) * 3500;
-    const ac1Tun = parseFloat(document.getElementById("ac 1 tun").value) * 2500;
-    const ledLight = parseFloat(document.getElementById("ledLight").value) * 10;
-    const machinery = parseFloat(document.getElementById("machinery").value) * 2000;
-    const freezer = parseFloat(document.getElementById("freezer").value) * 3000;
-    const bulbs = parseFloat(document.getElementById("bulbs").value) * 100;
-    const cameraLed = parseFloat(document.getElementById("cameraLed").value) * 100;
-    const fan = parseFloat(document.getElementById("fan").value) * 150;
-    const parkingLights = parseFloat(document.getElementById("parkingLights").value) * 200;
-    const ledDisplayBoard = parseFloat(document.getElementById("LED display Board").value) * 400;
-    const waterPump = parseFloat(document.getElementById("water pump").value) * 1000;
+    const ac1_5Tun = parseFloat(document.getElementById("ac 1.5 tun").value) * 3500 || 0;
+    const ac1Tun = parseFloat(document.getElementById("ac 1 tun").value) * 2500 || 0;
+    const ledLight = parseFloat(document.getElementById("ledLight").value) * 10 || 0;
+    const machinery = parseFloat(document.getElementById("machinery").value) * 2000 || 0;
+    const freezer = parseFloat(document.getElementById("freezer").value) * 3000 || 0;
+    const bulbs = parseFloat(document.getElementById("bulbs").value) * 100 || 0;
+    const cameraLed = parseFloat(document.getElementById("cameraLed").value) * 100 || 0;
+    const fan = parseFloat(document.getElementById("fan").value) * 150 || 0;
+    const chestFreezer = parseFloat(document.getElementById("chest freezer").value) * 2500 || 0;
+    const ledDisplayBoard = parseFloat(document.getElementById("LED display Board").value) * 400 || 0;
+    const waterPump = parseFloat(document.getElementById("water pump").value) * 1000 || 0;
 
-    const totalWatts = ac1_5Tun + ac1Tun + ledLight + machinery + freezer + bulbs + cameraLed + airConditionFan + parkingLights + ledDisplayBoard + waterPump;
+    const totalWatts = ac1_5Tun + ac1Tun + ledLight + machinery + freezer + bulbs + cameraLed + fan + chestFreezer + ledDisplayBoard + waterPump;
 
     const totalKilowattHours = totalWatts / 1000;
 
-    document.getElementById("totalWatts").innerText = totalKilowattHours.toFixed(2);
-}                                                                                                                                                                                                                                      
+    document.getElementById("totalWatts").innerText = totalKilowattHours.toFixed(2) + " kWh";
+}
